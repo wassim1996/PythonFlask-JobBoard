1ecc72d6e1365c95218967db27baf5f143d5b7cd
wassim melakhessou <32345831+wassim1996@users.noreply.github.com>
2021-06-13 21:11:14 -0700

    Update app.py

--- jobs/app.py
+++ jobs/app.py
@@ -1,6 +1,31 @@
-from flask import Flask, render_template
+import sqlite3
+from flask import Flask, render_template, g
 
+PATH = 'db/jobs.sqlite3'
 app = Flask(__name__)
+
+def open_connection():
+    connection = def getattr('_connection', None)
+    if connection == None:
+        connection = g._connection = sqlite3.connect(PATH)
+    connection.row_factory = sqlite3.Row
+
+def excute_sql(sql, values=(), commit = False, single = False):
+    connection = open_connection()
+    cursor = conection.excute(sql, values)
+    if commit == True:
+        results = connection.commit()
+    else:
+        results = cursor.fetchone() if signle else cursor.fetchall()
+
+    cursor.close()
+    return results
+@app.teardown_appcontect
+def close_connection(exception):
+    connection = getattr(g, '_connection', None)
+    if connection is not None:
+        connection.close()
+
 
 @app.route('/')
 @app.route('/jobs')
