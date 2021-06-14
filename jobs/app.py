ae2f7acb5f415e0c18bfee2a4d78541066923142
wassim melakhessou <32345831+wassim1996@users.noreply.github.com>
2021-06-13 21:14:01 -0700

    Update app.py

--- jobs/app.py
+++ jobs/app.py
@@ -20,7 +20,7 @@ def excute_sql(sql, values=(), commit = False, single = False):
 
     cursor.close()
     return results
-@app.teardown_appcontect
+@app.teardown_appcontext
 def close_connection(exception):
     connection = getattr(g, '_connection', None)
     if connection is not None:
